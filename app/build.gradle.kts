2493f64b40d39241a83637a1e9b60db65eb55289
apurba-gouda <42215600+apurba-gouda@users.noreply.github.com>
2026-06-04 15:40:10 +0530

    Fix KAPT JPMS compatibility issue with proper JVM toolchain

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,19 +31,24 @@ android {
             )
         }
     }
+    
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
     }
+    
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "11"
     }
+    
     buildFeatures {
         compose = true
     }
+    
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.3"
     }
+    
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
